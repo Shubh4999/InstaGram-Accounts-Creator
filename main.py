16fe89e77f29211d9de696af16f82b80c54fe632
Shubh Khandelwal <111572886+Shubh4999@users.noreply.github.com>
2026-02-02 18:27:04 +0530

    Update email for InstagramDeepLinkBot instance

--- main.py
+++ main.py
@@ -73,5 +73,5 @@ class InstagramDeepLinkBot:
             res_final = self.session.post(url_final, headers=final_headers, data=data_final)
             return res_final.json()
         return "Failed to get signup_token"
-bot = InstagramDeepLinkBot("shubhytprem@gmail.com")
+bot = InstagramDeepLinkBot("ytpremshubh@gmail.com")
 print(json.dumps(bot.run(), indent=2))
